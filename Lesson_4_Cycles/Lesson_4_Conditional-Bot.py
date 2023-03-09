c54d71e1d0b5be22056c21d80521b92e35588fef
Emil Dotsenko <emildocenko@gmail.com>
2023-03-10 01:53:27 +0300

    Test №2

--- Lesson_4_Cycles/Lesson_4_Conditional-Bot.py
+++ Lesson_4_Cycles/Lesson_4_Conditional-Bot.py
@@ -1,31 +1,40 @@
 line = str(input())
-line_s = line.strip("!;?:,.()")
+line_s = line.strip("!;?:,.()").lower().split()
 
 answer = ["привет", "хай", "здарова", "добрый день", "добрый вечер", "здравствуйте"]
 
-if line_s.lower() in answer:
-    print("Здравствуй, я бот с Украины!")
+if set(answer) & set(line_s):
+    print("Здравствуй, я бот с Украины! ")
 else:
-    print("Брат, я ничего не понимаю(")
+    print("Брат, я ничего не понимаю( ")
 
 line_two = str(input())
-line_two_s = line_two.strip("!;?:,.()")
+line_two_s = line_two.strip("!;?:,.()").lower().split()
 
-answer_two = ["как дела", "что делаешь", "здарова", "чем занимаешься", "занят", "свободен"]
+answer_two = ["как дела", "что делаешь", "как успехи", "чем занимаешься", "занят", "свободен"]
 
-if line_two_s.lower() in answer_two:
-    print("Учусь программировать на Python!")
+if set(answer_two) & set(line_two_s):
+    print("Учусь программировать на Python! ")
 else:
-    print("Брат, выражайся яснее")
+    print("Брат, выражайся яснее ")
 
 line_three = str(input())
-line_three_s = line_three.strip("!;?:,.()")
+line_three_s = line_three.strip("!;?:,.()").lower().split()
 
 answer_three = ["сериал", "кинотеатр", "фильм", "хобби", "делаю", "занимаюсь"]
 
-if line_three_s.lower() in answer_three:
-    print(f'Брат, я немного занят сейчас, но могу посоветовать тебе, {} это как раз соответствует твоим интересам.')
+if set(answer_three) & set(line_three_s):
+    if line_three_s.count("сериал") >= 1:
+        print("Брат, я немного занят сейчас, но могу посоветовать тебе сериал 'The Sopranos' ")
+    elif line_three_s.count("кинотеатр") >= 1:
+        print("Брат, я немного занят сейчас, но могу посоветовать тебе сходить на 'Мавка: Лісова пісня' ")
+    elif line_three_s.count("фильм") >= 1:
+        print("Брат, я немного занят сейчас, но могу посоветовать тебе сходить на 'The Hateful Eight' ")
+    elif line_three_s.count("хобби") >= 1:
+        print("Брат, я немного занят сейчас, но могу посоветовать тебе такое хобби, как 'Warhammer' ")
+    elif line_three_s.count("кинотеатр") >= 1:
+        print("Брат, я немного занят сейчас, давай потом ")
+    else:
+        print("Брат, я немного занят сейчас, но очень интересно! ")
 else:
-    print("Что, простите?")
-
-
+    print("Что, прости? ")
