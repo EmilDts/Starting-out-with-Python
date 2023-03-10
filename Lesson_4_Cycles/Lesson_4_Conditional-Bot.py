7259bb4e6c564e885695a6457896cb21a7da85c2
Emil Dotsenko <emildocenko@gmail.com>
2023-03-10 05:53:38 +0300

    Legendary Edition

--- Lesson_4_Cycles/Lesson_4_Conditional-Bot.py
+++ Lesson_4_Cycles/Lesson_4_Conditional-Bot.py
@@ -1,52 +1,52 @@
-line = str(input())
-line_s = line.strip("!;?:,.()").lower().split()
+line = str(input()).strip("!;?:,.()").lower().split()
 
 answer = ["привет", "хай", "здарова", "добрый день", "добрый вечер", "здравствуйте"]
 
-if set(answer) & set(line_s):
-    print("Здравствуй, я бот с Украины! ")
-else:
+while not set(answer) & set(line):
     print("Брат, я ничего не понимаю( ")
+    line = str(input()).strip("!;?:,.()").lower().split()
+else:
+    print("Здравствуй, я бот с Украины! ")
 
-line_two = str(input())
-line_two_s = line_two.strip("!;?:,.()").lower().split()
+line_two = str(input()).strip("!;?:,.()").lower().split()
 
 answer_two = ["как дела", "что делаешь", "как успехи", "чем занимаешься", "занят", "свободен"]
 
-if set(answer_two) & set(line_two_s):
-    print("Учусь программировать на Python! ")
-else:
+while not set(answer_two) & set(line_two):
     print("Брат, выражайся яснее ")
+    line_two = str(input()).strip("!;?:,.()").lower().split()
+else:
+    print("Учусь программировать на Python! ")
 
-line_three = str(input())
-line_three_s = line_three.strip("!;?:,.()").lower().split()
+line_three = str(input()).strip("!;?:,.()").lower().split()
 
 answer_three = ["сериал", "кинотеатр", "фильм", "хобби", "делаю", "занимаюсь"]
 
-if set(answer_three) & set(line_three_s):
-    if line_three_s.count("сериал") >= 1:
+while not set(answer_three) & set(line_three):
+    print("Что, прости? ")
+    line_three = str(input()).strip("!;?:,.()").lower().split()
+else:
+    if line_three.count("сериал") >= 1:
         print("Брат, я немного занят сейчас, но могу посоветовать тебе сериал 'The Sopranos' ")
-    elif line_three_s.count("кинотеатр") >= 1:
+    elif line_three.count("кинотеатр") >= 1:
         print("Брат, я немного занят сейчас, но могу посоветовать тебе сходить на 'Мавка: Лісова пісня' ")
-    elif line_three_s.count("фильм") >= 1:
+    elif line_three.count("фильм") >= 1:
         print("Брат, я немного занят сейчас, но могу посоветовать тебе сходить на 'The Hateful Eight' ")
-    elif line_three_s.count("хобби") >= 1:
+    elif line_three.count("хобби") >= 1:
         print("Брат, я немного занят сейчас, но могу посоветовать тебе такое хобби, как 'Warhammer' ")
-    elif line_three_s.count("кинотеатр") >= 1:
+    elif line_three.count("кинотеатр") >= 1:
         print("Брат, я немного занят сейчас, давай потом ")
     else:
         print("Брат, я немного занят сейчас, но очень интересно! ")
+
+print("Слушай, мне реально пора... /n")
+
+line_four = str(input()).strip("!;?:,.()").lower().split()
+
+answer_four = ["пока", "увидимся", "адьес", "гудбай", "поки", "свидимся"]
+
+while not set(answer_four) & set(line_four):
+    print("Я устал... Оставь меня в покое... ")
+    line_four = str(input()).strip("!;?:,.()").lower().split()
 else:
-    print("Что, прости? ")
-
-line_four = str(input())
-line_four_s = line_four.strip("!;?:,.()").lower().split()
-
-answer_four = ["пока", "до завтра", "до свидания", "увидимся", "до встречи", "спокойной ночи"]
-
-if set(answer_four) & set(line_four_s):
-    print("До встречи в сети! ")
-else:
-    print("Все равно ничего не понял... ")
-
-exit()
+    exit(print("До встречи в сети. "))
