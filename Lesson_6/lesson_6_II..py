3649b296812e2deb072395209010d65a5337e49e
Emil Dotsenko <emildocenko@gmail.com>
2023-03-16 01:58:02 +0300

    Test#2.

--- Lesson_6/lesson_6_II..py
+++ Lesson_6/lesson_6_II..py
@@ -7,6 +7,6 @@ while "(" in order:
     while order[passport] != ")":
         order.pop(passport)
     order.remove(")")
-    if order[passport-1] == " ":
-        order.pop(passport-1)
+    if order[passport - 1] == " ":
+        order.pop(passport - 1)
 print(''.join(order))
