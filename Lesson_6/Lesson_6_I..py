3bcf140f762ba6ba9ad29774b0a02e86d649ff1a
Emil Dotsenko <emildocenko@gmail.com>
2023-03-16 12:35:01 +0300

    Test#3.

--- Lesson_6/Lesson_6_I..py
+++ Lesson_6/Lesson_6_I..py
@@ -1,6 +1,10 @@
+# The user enters a string:
 line = str(input("Введите вашу строку: ")).strip("!;?:,.() \n\t").lower()
 
+# If the user string is not equal to the back end string:
 while not line == list[::-1]:
+
+    # True is executed and the user is asked to enter the string again:
     print(f'Строка "{line.capitalize()}" не является палиндромом "{line[::-1].capitalize()}"')
     line = str(input("Введите вашу строку: ")).strip("!;?:,.()").lower()
 else:
