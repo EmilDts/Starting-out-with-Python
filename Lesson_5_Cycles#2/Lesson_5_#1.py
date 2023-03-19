9b84fe12cfb3e0a6d89cd6b0c24619b32b20d3ac
Emil Dotsenko <emildocenko@gmail.com>
2023-03-19 03:42:23 +0300

    Test#4.

--- Lesson_5_Cycles#2/Lesson_5_#1.py
+++ Lesson_5_Cycles#2/Lesson_5_#1.py
@@ -1,13 +1,11 @@
-line = float(input("Введите числа для подсчета. Когда закончите, то введите ключевое слово 'sum'. \n"))
+ere_we_go = float(input("Введите числа для подсчета. Когда закончите, то введите ключевое слово 'sum'. \n"))
 
 memory = []
 
 while True:
-    while type(line) == float:
+    while type(ere_we_go) == float:
         try:
-            memory.append(line)
-            line = float(input("Еще: \n"))
+            memory.append(ere_we_go)
+            ere_we_go = float(input("Еще: \n"))
         except ValueError:
-            print(sum(memory))
-            exit()
-
+            print(exit(sum(memory)))
