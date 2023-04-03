49a4d98ab1ceb49c873664668bd3c82f78cce65f
Emil Dotsenko <emildocenko@gmail.com>
2023-04-03 09:41:03 +0300

    Test#2.

--- Lesson_11/Lesson_11_DC.py
+++ Lesson_11/Lesson_11_DC.py
@@ -1,11 +1,14 @@
+# We create a function in which we write a text file split into a list:
 def word_processing(f_handler) -> list:
     lines = list()
     for line in f_handler.readlines():
-        lines.append(line.strip())
+        # Returning values with a capital letter:
+        lines.append(line.strip().capitalize())
     return lines
 
 
 if __name__ == '__main__':
+    # Calling the file handler for reading:
     f = open('Shakespeare_Sonnet_#1.txt', 'r', encoding='utf-8')
 
     print("Эта программа читает текстовый файл и создает из строк список.")
